5ef18461599224ab80ea08ec2e7368b9739590d6
felipe152 <peixoto152@gmail.com>
2020-06-23 22:27:44 -0300

    add mailchimp code

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 		<footer>
 			
 		</footer>
-		<?php wp_footer(); ?>      
+		<?php wp_footer(); ?>
+		<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/e31a51e62c47813c224a8d648/3a78c03bebf410c6a7516a115.js");</script>  
     </body>
   </html>
